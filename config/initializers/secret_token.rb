6ab28cebbf4bc9bd54d34fa04e03923e41dfc5a8
Donny Kurnia <donnykurnia@gmail.com>
2012-04-18 18:52:01 +0700

    initial commit
    Signed-off-by: Donny Kurnia <donnykurnia@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WriterAppRor::Application.config.secret_token = 'e87373e4ad6347e82292f074cb8f135acc03fdaf8a1c9c9c907e51adc486bc6dd72c9dacaad0be3989ef877d5f15c57c6316018c061fb05a21ce496e59bd8d76'
