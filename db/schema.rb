c630fc6d71a5b0f86106ccd08a08069254ac340b
Donny Kurnia <donnykurnia@gmail.com>
2012-04-19 08:04:22 +0700

    update schema.rb
    Signed-off-by: Donny Kurnia <donnykurnia@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120418184104) do
+ActiveRecord::Schema.define(:version => 20120419010214) do
 
   create_table "teams", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(:version => 20120418184104) do
     t.datetime "updated_at", :null => false
     t.datetime "deleted_at"
   end
+
+  add_index "teams", ["deleted_at"], :name => "index_teams_on_deleted_at"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -40,6 +42,7 @@ ActiveRecord::Schema.define(:version => 20120418184104) do
     t.datetime "deleted_at"
   end
 
+  add_index "users", ["deleted_at"], :name => "index_users_on_deleted_at"
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
   add_index "users", ["role"], :name => "index_users_on_role"
