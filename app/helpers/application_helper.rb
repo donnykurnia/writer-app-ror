4ec7101712f53c0dae85f276a4101b3265ae8668
Donny Kurnia <donnykurnia@gmail.com>
2012-04-19 12:01:10 +0700

    refactor displaying flash messages
    Signed-off-by: Donny Kurnia <donnykurnia@gmail.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def display_flash(name,msg)
+    case name.to_s
+    when "notice" then
+      content_tag :div, :class => "alert alert-success fade in" do
+        link_to('&times;'.html_safe, '#', :class => "close", :"data-dismiss" => "alert") +
+        '<i class="icon-ok-sign"></i> '.html_safe +
+        msg
+      end
+    when "alert" then
+      content_tag :div, :class => "alert alert-warning fade in" do
+        link_to('&times;'.html_safe, '#', :class => "close", :"data-dismiss" => "alert") +
+        '<i class="icon-warning-sign"></i> '.html_safe +
+        msg
+      end
+    else
+      content_tag :div, :class => "alert alert-info fade in" do
+        link_to('&times;'.html_safe, '#', :class => "close", :"data-dismiss" => "alert") +
+        '<i class="icon-info-sign"></i> '.html_safe +
+        msg
+      end
+    end
+  end
 end
