ac3a0a1f0ed008540d2c1d0a80c71d5c8111304d
Donny Kurnia <donnykurnia@gmail.com>
2012-04-20 00:59:04 +0700

    allow login using either username or email
    Signed-off-by: Donny Kurnia <donnykurnia@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :full_name, :role
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :login, :full_name, :role
+  attr_accessor :login
 
   belongs_to :team
   has_many :projects
@@ -16,6 +17,12 @@ class User < ActiveRecord::Base
 
   def name
     self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    login = conditions.delete(:login)
+    where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.strip.downcase }]).first
   end
 
 end
