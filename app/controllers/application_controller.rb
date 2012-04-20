869b8c4fa971f0085a59023052c8ad15deb4f02d
Donny Kurnia <donnykurnia@gmail.com>
2012-04-20 23:06:53 +0700

    add rescue for ActiveRecord::RecordNotFound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
+
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    redirect_to root_url, :alert => 'Data not found!'
+  end
 end
