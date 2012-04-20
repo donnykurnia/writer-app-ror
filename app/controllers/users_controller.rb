0062e73dda5de332f892d0630588021036cfddbc
Donny Kurnia <donnykurnia@gmail.com>
2012-04-20 18:49:51 +0700

    refactor destroy method
    Signed-off-by: Donny Kurnia <donnykurnia@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,11 +71,14 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy
-
     respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
+      if @user.destroy
+        format.html { redirect_to users_url, notice: "User '#{@user.name}' was successfully deleted." }
+        format.json { head :no_content }
+      else
+        format.html { redirect_to users_url, alert: "Failed to delete '#{@user.name}'." }
+        format.json { head :no_content }
+      end
     end
   end
 
