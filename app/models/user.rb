5e97823653c56bf599bb5ff44c1b50d708617f86
Donny Kurnia <donnykurnia@gmail.com>
2012-04-20 11:52:22 +0700

    disable registerable
    Signed-off-by: Donny Kurnia <donnykurnia@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,18 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
+         #:registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :login, :full_name, :role, :team_id
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :login, :full_name, :role, :team_id, :team_attributes
   attr_accessor :login
 
   belongs_to :team
   has_many :projects
+
+  accepts_nested_attributes_for :team
 
   delegate :name, :to => :team, :prefix => true
 
