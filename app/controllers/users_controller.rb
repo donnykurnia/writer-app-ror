1cb872f77702dac9efd0c2f67eb5f0ca22a397af
Donny Kurnia <donnykurnia@gmail.com>
2012-04-20 08:54:00 +0700

    refactor user controller
    Signed-off-by: Donny Kurnia <donnykurnia@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :find_user, :only => [:show, :edit, :update, :destroy]
+
   # GET /users
   # GET /users.json
   def index
@@ -14,8 +16,6 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @user = User.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @user }
@@ -35,7 +35,6 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
-    @user = User.find(params[:id])
   end
 
   # POST /users
@@ -57,8 +56,6 @@ class UsersController < ApplicationController
   # PUT /users/1
   # PUT /users/1.json
   def update
-    @user = User.find(params[:id])
-
     respond_to do |format|
       if @user.update_attributes(params[:user])
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
@@ -73,7 +70,6 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
 
     respond_to do |format|
@@ -81,4 +77,11 @@ class UsersController < ApplicationController
       format.json { head :no_content }
     end
   end
+
+  protected
+
+    def find_user
+      @user = User.find(params[:id])
+    end
+
 end
