3c57122078960cddd91f0d68d823282767f546d2
Donny Kurnia <donnykurnia@gmail.com>
2012-04-21 06:54:12 +0700

    add validations and callback
    Signed-off-by: Donny Kurnia <donnykurnia@gmail.com>

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,32 @@
 class Project < ActiveRecord::Base
   attr_accessible :title, :description, :status, :deadline, :creator_id, :team_id, :total_cost
 
+  STATUSES = ['open', 'in progress', 'finished', 'expired']
+
   belongs_to :team, :inverse_of => :projects
   belongs_to :creator, :class_name => "User",
     :foreign_key => "creator_id", :inverse_of => :projects
 
+  before_destroy :check_deleteable
+
   acts_as_paranoid
+
+  validates :title, :presence => true
+  validates :description, :presence => true
+  validates :total_cost, :presence => true, :numericality => true
+  validates :status, :inclusion => { :in => self::STATUSES,
+    :message => "%{value} is not a valid status" }
+  validates :deadline, :presence => true
+
+  def deleteable?
+    true
+    #self.bids.count == 0
+  end
+
+  protected
+
+    def check_deleteable
+      self.deleteable?
+    end
+
 end
