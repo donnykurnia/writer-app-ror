77957d16e8b5cf384d17ef8b42383bf667a9e065
Donny Kurnia <donnykurnia@gmail.com>
2012-04-21 06:36:35 +0700

    add current_team helper_method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   check_authorization :unless => :devise_controller?
+  helper_method :current_team
+
+  def current_team
+    @current_team ||= current_user.team
+  end
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
