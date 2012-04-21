1e741b620dd5050d172a0ee35a88c463f4f744f6
Donny Kurnia <donnykurnia@gmail.com>
2012-04-21 07:59:35 +0700

    add delegate
    Signed-off-by: Donny Kurnia <donnykurnia@gmail.com>

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,6 +7,9 @@ class Project < ActiveRecord::Base
   belongs_to :creator, :class_name => "User",
     :foreign_key => "creator_id", :inverse_of => :projects
 
+  delegate :name, :to => :creator, :prefix => true
+  delegate :name, :to => :team, :prefix => true
+
   before_destroy :check_deleteable
 
   acts_as_paranoid
