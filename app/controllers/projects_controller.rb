ce7d78d55cf773ed601d24c65b707d97abdb2d03
Donny Kurnia <donnykurnia@gmail.com>
2012-04-25 00:07:19 +0700

    use respond_with in controller
    Signed-off-by: Donny Kurnia <donnykurnia@gmail.com>

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,79 +7,46 @@ class ProjectsController < ApplicationController
   # GET /projects.json
   def index
     @projects = @projects.page(params[:page])
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @projects }
-    end
+    respond_with @projects
   end
 
   # GET /projects/1
   # GET /projects/1.json
   def show
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @project }
-    end
+    respond_with @project
   end
 
   # GET /projects/new
   # GET /projects/new.json
   def new
     @project = current_user.projects.build({:status => 'open', :deadline => (Time.now + 7.days)})
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @project }
-    end
+    respond_with @project
   end
 
   # GET /projects/1/edit
   def edit
+    respond_with @project
   end
 
   # POST /projects
   # POST /projects.json
   def create
-    @project = current_user.projects.build(params[:project])
-
-    respond_to do |format|
-      if @project.save
-        format.html { redirect_to @project, notice: 'Project was successfully created.' }
-        format.json { render json: @project, status: :created, location: @project }
-      else
-        format.html { render :new }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
-    end
+    @project = current_user.projects.create(params[:project])
+    respond_with @project
   end
 
   # PUT /projects/1
   # PUT /projects/1.json
   def update
-    respond_to do |format|
-      if @project.update_attributes(params[:project])
-        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render :edit }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
-    end
+    @project.update_attributes(params[:project])
+    respond_with @project
   end
 
   # DELETE /projects/1
   # DELETE /projects/1.json
   def destroy
-    respond_to do |format|
-      if @project.destroy
-        format.html { redirect_to projects_url, notice: "Project '#{@project.title}' was successfully deleted." }
-        format.json { head :no_content }
-      else
-        format.html { redirect_to projects_url, alert: "Failed to delete project '#{@project.title}'." }
-        format.json { head :no_content }
-      end
-    end
+    @project.destroy
+    respond_with @project
   end
 
 end
