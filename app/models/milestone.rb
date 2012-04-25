5a28cd335743bebd89d257a85541d06bc1cdd839
Donny Kurnia <donnykurnia@gmail.com>
2012-04-25 20:14:16 +0700

    add methods to milestone model, update validation
    Signed-off-by: Donny Kurnia <donnykurnia@gmail.com>

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -6,16 +6,42 @@ class Milestone < ActiveRecord::Base
   delegate :title, :to => :project, :prefix => true
   delegate :name, :to => :writer, :prefix => true
 
-  STATUSES = ['open', 'finished', 'release request', 'released', 'paid']
+  STATUSES = ['open', 'finished', 'release requested', 'released', 'paid']
 
   attr_accessible :title, :description, :amount, :status, :writer_id, :payment_time, :payment_via
+
+  before_destroy :check_deleteable
+  before_create :set_status
 
   acts_as_paranoid
 
   validates :project, :presence => true
   validates :title, :presence => true
   validates :amount, :presence => true, :numericality => true
-  validates :status, :inclusion => { :in => self::STATUSES,
+  validates :status, :on => :update, :inclusion => { :in => self::STATUSES,
     :message => "%{value} is not a valid status" }
   validates :payment_time, :allow_blank => true, :timeliness => {:type => :date}
+
+  def to_s
+    self.title
+  end
+
+  def deleteable?
+    self.writer_id.nil? || self.status == 'open'
+  end
+
+  protected
+
+    def check_deleteable
+      if self.deleteable?
+        true
+      else
+        errors[:base] << "Delete failed"
+        false
+      end
+    end
+
+    def set_status
+      self.status = 'open'
+    end
 end
