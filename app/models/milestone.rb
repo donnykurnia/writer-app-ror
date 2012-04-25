444c6da586196850b3602e470c41a4fd6944a4f5
Donny Kurnia <donnykurnia@gmail.com>
2012-04-25 19:09:20 +0700

    add validation
    Signed-off-by: Donny Kurnia <donnykurnia@gmail.com>

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -11,4 +11,11 @@ class Milestone < ActiveRecord::Base
   attr_accessible :title, :description, :amount, :status, :writer_id, :payment_time, :payment_via
 
   acts_as_paranoid
+
+  validates :project, :presence => true
+  validates :title, :presence => true
+  validates :amount, :presence => true, :numericality => true
+  validates :status, :inclusion => { :in => self::STATUSES,
+    :message => "%{value} is not a valid status" }
+  validates :payment_time, :allow_blank => true, :timeliness => {:type => :date}
 end
