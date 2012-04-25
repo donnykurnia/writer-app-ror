4b479606a9e5764d28c334270c59027cedec46f1
Donny Kurnia <donnykurnia@gmail.com>
2012-04-25 10:13:20 +0700

    reorder model attributes, add milestone association
    Signed-off-by: Donny Kurnia <donnykurnia@gmail.com>

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,15 +1,16 @@
 class Project < ActiveRecord::Base
-  attr_accessible :title, :description, :status, :deadline, :creator_id, :team_id, :total_cost
-
-  STATUSES = ['open', 'in progress', 'finished', 'expired']
-
   belongs_to :team, :inverse_of => :projects
   belongs_to :creator, :class_name => "User",
     :foreign_key => "creator_id", :inverse_of => :projects
+  has_many :milestones, :inverse_of => :project
 
   delegate :name, :to => :creator, :prefix => true
   delegate :name, :to => :team, :prefix => true
   delegate :writers, :to => :team, :prefix => true
+
+  STATUSES = ['open', 'in progress', 'finished', 'expired']
+
+  attr_accessible :title, :description, :status, :deadline, :creator_id, :team_id, :total_cost
 
   before_destroy :check_deleteable
   before_create :set_team
